5bdb4dc389178bdf526642f8ddbf0b1d05f2a344
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-06-10 19:13:08 +0700

    Campaign repository

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"crowdfunding/auth"
+	"crowdfunding/campaign"
 	"crowdfunding/handler"
 	"crowdfunding/helper"
 	"crowdfunding/user"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -24,6 +26,15 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, _ := campaignRepository.FindByUserID(1)
+	for _, campaign := range campaigns {
+		fmt.Println(campaign)
+	}
+
+	// campaigns := campaignRepository.FindByUserID(1)
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
